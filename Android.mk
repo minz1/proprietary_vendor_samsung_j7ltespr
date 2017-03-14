1fbbb2aff8fd4b99a0d929871d9549a3e2b1e600
minz1 <minz1@github.com>
2017-03-14 18:20:07 -0400

    j7ltespr: fix errors for good

--- Android.mk
+++ Android.mk
@@ -18,6 +18,15 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),j7ltespr)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
