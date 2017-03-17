fd2d36f5ea83a51a3a7e9b6232f5d9ccd1ff2999
minz1 <minz1@github.com>
2017-03-17 14:36:09 -0400

    j7ltespr: redo j7ltespr-vendor-blobs.mk

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ ifneq ($(filter j7ltespr,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/extra/vendor/lib/libtime_genoff.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
@@ -32,7 +32,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/extra/app/TimeService/TimeService.apk
+LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
@@ -42,7 +42,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libHevcSwDecoder
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/extra/vendor/lib/libHevcSwDecoder.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/libHevcSwDecoder.so
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
@@ -52,7 +52,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmm-abl
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/extra/vendor/lib/libmm-abl.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/libmm-abl.so
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
